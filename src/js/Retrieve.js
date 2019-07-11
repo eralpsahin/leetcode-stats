e6dd018c40a8b1248067f91f8abd5eac5f2ea8b5
Eralp Sahin <eralpsahin@me.com>
2019-07-11 10:49:47 -0400

    Fix cors anywhere on production

--- src/js/Retrieve.js
+++ src/js/Retrieve.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
-
-const instance = axios.create({
-  headers: { 'Access-Control-Allow-Origin': '*' }
-});
+import isDev from 'electron-is-dev';
 
 const Retrieve = {};
+const instance = axios.create();
+// Enable Cors Anywhere for development
+if (isDev) {
+  instance.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/';
+} else {
+  instance.defaults.baseURL = 'https://';
+}
 
 /**
  * This function retrieves all the data from users profile.
@@ -13,17 +17,11 @@ const Retrieve = {};
  * @returns {Number} Error code of the response
  */
 Retrieve.get = async username => {
-  const response = await instance.get(
-    `https://cors-anywhere.herokuapp.com/https://leetcode.com/${username}`
-  );
+  const response = await instance.get(`leetcode.com/${username}`);
   const $ = cheerio.load(response.data);
-
   return {
     avatar: $('.img-rounded').attr('src'),
-    realname:
-      $('.realname').attr('title').length <= 1
-        ? 'LeetCode Stats'
-        : $('.realname').attr('title')
+    realname: $('.realname').attr('title')
   };
 };
 
